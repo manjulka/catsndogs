c5d6914c86f7367639bd9d04580a60d6146a6eba
manjulka <raketchica0@gmail.com>
2016-06-07 21:49:07 +0300

    Update Dog.java
    добавить класс енимал

--- src/dogs/Dog.java
+++ src/dogs/Dog.java
@@ -1,14 +1,14 @@
 package dogs;
 
-
+import main.Animal;
 import java.util.Scanner;
 
-public class Dog implements IDog extends Animal{
-    public static String dogName;
+public class Dog extends Animal implements IDog{
+    public String dogName;
 
-    public void doBark(dogName){
+    public void doBark(){
         Scanner ind = new Scanner(System.in);
-        dogName = ind.nextLine("Dog name: ");
+        dogName = ind.nextLine();
         System.out.println("My name is " + dogName + ". Bark bark");
     }
 }
