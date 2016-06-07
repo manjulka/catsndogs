bfde158d1a4edd2d5afb8292dd1bd8dc95eae7c0
manjulka <raketchica0@gmail.com>
2016-06-07 21:52:30 +0300

    Update Food.java

--- src/main/Food.java
+++ src/main/Food.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 public class Food extends Animal implements IFood {
 
-    public static int foodWeight;
+    public int foodWeight;
 
-    public static int getDozeWeight(){
+    public int getDozeWeight(){
 
         Scanner in = new Scanner(System.in);
         foodWeight = in.nextInt();
